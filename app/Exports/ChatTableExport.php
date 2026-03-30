5a2c528e5a655ae9affe888da27a0de3bfc80283
awand795 <awand795@gmail.com>
2026-03-30 07:59:56 +0700

    export chart ke excel

--- app/Exports/ChatTableExport.php
+++ app/Exports/ChatTableExport.php
@@ -3,12 +3,20 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithCharts;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Chart\Chart;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeries;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
+use PhpOffice\PhpSpreadsheet\Chart\Legend;
+use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
+use PhpOffice\PhpSpreadsheet\Chart\Title as ChartTitle;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ChatTableExport implements FromArray, WithHeadings, WithStyles, WithTitle
+class ChatTableExport implements FromArray, WithHeadings, WithStyles, WithTitle, WithCharts, WithCustomStartCell
 {
     protected $headers;
     protected $rows;
@@ -27,6 +35,15 @@ class ChatTableExport implements FromArray, WithHeadings, WithStyles, WithTitle
         $this->rows = $rows;
         $this->title = substr($title, 0, 31); // Excel sheet title max 31 chars
         $this->chartInfo = $chartInfo;
+    }
+
+    /**
+     * @return string
+     */
+    public function startCell(): string
+    {
+        // If there is a chart, start data at row 25 to leave space for chart at top
+        return $this->chartInfo ? 'A25' : 'A1';
     }
 
     /**
@@ -52,13 +69,73 @@ class ChatTableExport implements FromArray, WithHeadings, WithStyles, WithTitle
      */
     public function styles(Worksheet $sheet)
     {
-        // Simple header styling only
+        $startRow = $this->chartInfo ? 25 : 1;
         $lastCol = $sheet->getHighestColumn();
         
-        $sheet->getStyle("A1:{$lastCol}1")->applyFromArray([
+        $sheet->getStyle("A{$startRow}:{$lastCol}{$startRow}")->applyFromArray([
             'font' => ['bold' => true],
             'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'F53003']],
         ]);
+    }
+
+    /**
+     * @return Chart|Chart[]
+     */
+    public function charts()
+    {
+        if (!$this->chartInfo) {
+            return [];
+        }
+
+        $type = $this->chartInfo['type'] ?? 'bar';
+        $title = $this->chartInfo['title'] ?? 'Chart Data';
+        $dataPoints = $this->chartInfo['dataPoints'] ?? count($this->rows);
+        $datasetCount = $this->chartInfo['datasetCount'] ?? 1;
+
+        // Map internal types to PhpSpreadsheet types
+        $chartType = DataSeries::TYPE_BARCHART;
+        if ($type === 'line') $chartType = DataSeries::TYPE_LINECHART;
+        if ($type === 'pie') $chartType = DataSeries::TYPE_PIECHART;
+        if ($type === 'doughnut') $chartType = DataSeries::TYPE_DONUTCHART;
+
+        // Data starts at row 26 (header is at 25)
+        // Labels are in column B (A is 'No', B is 'Label')
+        $categories = [
+            new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, "'{$this->title}'!\$B\$26:\$B$" . (25 + $dataPoints), null, $dataPoints),
+        ];
+
+        $values = [];
+        $labels = [];
+        for ($i = 0; $i < $datasetCount; $i++) {
+            $colLetter = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex(3 + $i); // Starting from column C
+            $labels[] = new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, "'{$this->title}'!\${$colLetter}\$25", null, 1);
+            $values[] = new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, "'{$this->title}'!\${$colLetter}\$26:\${$colLetter}\$" . (25 + $dataPoints), null, $dataPoints);
+        }
+
+        $series = new DataSeries(
+            $chartType,
+            DataSeries::GROUPING_CLUSTERED,
+            range(0, count($values) - 1),
+            $labels,
+            $categories,
+            $values
+        );
+
+        $plotArea = new PlotArea(null, [$series]);
+        $legend = new Legend(Legend::POSITION_RIGHT, null, false);
+        $chartTitle = new ChartTitle($title);
+
+        $chart = new Chart(
+            'chart1',
+            $chartTitle,
+            $legend,
+            $plotArea
+        );
+
+        $chart->setTopLeftPosition('A1');
+        $chart->setBottomRightPosition('L23');
+
+        return $chart;
     }
 
     /**
