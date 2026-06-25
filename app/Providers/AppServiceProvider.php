da40a87ea960600dc3ffda321d1784b7302dca7a
awand795 <awand795@gmail.com>
2026-06-25 14:03:12 +0700

    fix: Add Octane database reconnect listener and install octane dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Octane Database Reconnect to prevent "has gone away" or dropped connections
+        if (class_exists(\Laravel\Octane\Events\RequestReceived::class)) {
+            Event::listen(\Laravel\Octane\Events\RequestReceived::class, function () {
+                $connections = ['pgsql', 'pgsql_mbi'];
+                
+                foreach ($connections as $connection) {
+                    try {
+                        if (DB::connection($connection)->getPdo()) {
+                            DB::connection($connection)->getPdo()->query('SELECT 1');
+                        }
+                    } catch (\Exception $e) {
+                        DB::connection($connection)->reconnect();
+                    }
+                }
+            });
+        }
     }
 }
